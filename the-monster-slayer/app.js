192b1dcbd770545f505ad80d840614e7fa37ece0
lericardolima <lericardolima@gmail.com>
2020-04-08 15:57:06 -0300

    Implementar botão 'Heal'

--- the-monster-slayer/app.js
+++ the-monster-slayer/app.js
@@ -18,7 +18,12 @@ new Vue({
             this.playerAttacks(10, 20);
         },
         heal: function() {
-
+            if (this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+            this.monsterAttacks();
         },
         giveUp: function() {
 
