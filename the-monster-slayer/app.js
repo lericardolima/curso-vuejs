bc5aee2295b8c2a2df02f06ebd7c97d09ff76dc9
lericardolima <lericardolima@gmail.com>
2020-04-08 17:04:00 -0300

    Implementar botão 'give up'!

--- the-monster-slayer/app.js
+++ the-monster-slayer/app.js
@@ -26,7 +26,7 @@ new Vue({
             this.monsterAttacks();
         },
         giveUp: function() {
-
+            this.gameIsRunning = false;
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
