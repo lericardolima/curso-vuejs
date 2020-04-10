e837eabe44dc1a1b4cfab09cb6167e4cf392b10f
lericardolima <lericardolima@gmail.com>
2020-04-09 22:54:01 -0300

    Implementar desafio de componentes iniciais

---
+++ server-manager/src/Server.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <ul class="list-group">
+      <li v-for="(number, index) in 5" :key="index" class="list-group-item">
+        <p>Server #{{ number }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {};
+</script>
--- server-manager/src/main.js
+++ server-manager/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Server from './Server'
+
+Vue.component('app-server', Server);
 
 new Vue({
   el: '#app',
