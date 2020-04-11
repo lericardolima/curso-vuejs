3a11e80e6fadc8641768ad4bdafad6d4011dbebc
lericardolima <lericardolima@gmail.com>
2020-04-11 12:39:43 -0300

    Implementar exercicio de formulários

---
+++ form-challenge/src/components/FullNameForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="form-horizontal">
+    <div class="col form-group">
+      <div class="col-xs-5">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="First name"
+          v-model="firstName"
+          @keyup="fullNameEdited"
+        />
+      </div>
+      <div class="col-xs-7">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Last name"
+          v-model="lastName"
+          @keyup="fullNameEdited"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data: () => {
+    return {
+      firstName: "",
+      lastName: ""
+    };
+  },
+  methods: {
+    fullNameEdited() {
+      this.$emit("input", `${this.firstName} ${this.lastName}`);
+    }
+  }
+};
+</script>
