ebcaef9dea64ce79d66ef03ed9a077920233696c
lericardolima <lericardolima@gmail.com>
2020-04-13 23:20:36 -0300

    Configurar Bootstrap

--- stock-trader/src/main.js
+++ stock-trader/src/main.js
@@ -2,9 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import routes from './routes'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Bootstrap configuration
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false
 
+// Router configuration
 Vue.use(VueRouter);
 
 const router = new VueRouter({
