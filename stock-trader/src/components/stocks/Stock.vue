d0e33d648197b35f28faefbe2c34b8ebb72bfbf6
lericardolima <lericardolima@gmail.com>
2020-04-14 13:55:53 -0300

    Componente para o stock

--- stock-trader/src/components/stocks/Stock.vue
+++ stock-trader/src/components/stocks/Stock.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>
-        <h1>Stocks Stock</h1>
+  <div class="m-3 col-sm-6 col-md-4">
+    <div class="card">
+      <div class="card-header">
+          NAME
+          <small>(Price: PRICE)</small>
+      </div>
+      <div class="d-flex card-body">
+          <div class="float-left">
+              <input type="number" class="form-control" placeholder="Quantity">
+          </div>
+          <div class="float-right mx-2">
+              <button class="btn btn-success">Buy</button>
+          </div>
+      </div>
     </div>
+  </div>
 </template>
--- stock-trader/src/components/stocks/Stocks.vue
+++ stock-trader/src/components/stocks/Stocks.vue
@@ -1,6 +1,24 @@
 <template>
-    <div>
-        <h1>Stocks</h1>
-        <router-view></router-view>
-    </div>
+  <div>
+    <h1>Stocks</h1>
+    <stock-card v-for="(stock, index) in stocks" :key="index"></stock-card>
+  </div>
 </template>
+<script>
+import Stock from "./Stock.vue";
+export default {
+  components: {
+    "stock-card": Stock
+  },
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "BMW", price: 100 },
+        { id: 2, name: "Google", price: 450 },
+        { id: 3, name: "Apple", price: 600 },
+        { id: 4, name: "Twitter", price: 300 }
+      ]
+    };
+  }
+};
+</script>
