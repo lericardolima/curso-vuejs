e33f9f77ebb68f8ccb4cfcecb540b96567f2ae0e
lericardolima <lericardolima@gmail.com>
2020-04-14 17:54:07 -0300

    Usar props para passar os dados para outro componente

--- stock-trader/src/components/stocks/Stock.vue
+++ stock-trader/src/components/stocks/Stock.vue
@@ -2,17 +2,43 @@
   <div class="col-sm-6 col-md-4">
     <div class="card">
       <div class="card-header">
-          NAME
-          <small>(Price: PRICE)</small>
+        {{ stock.name }}
+        <small>(Price: {{ stock.price }})</small>
       </div>
       <div class="d-flex card-body">
-          <div class="float-left">
-              <input type="number" class="form-control" placeholder="Quantity">
-          </div>
-          <div class="float-right mx-2">
-              <button class="btn btn-success">Buy</button>
-          </div>
+        <div class="float-left">
+          <input type="number" class="form-control" placeholder="Quantity" v-model="quantity"/>
+        </div>
+        <div class="float-right mx-2">
+          <button class="btn btn-success" :disabled="!isValidQuantity">Buy</button>
+        </div>
       </div>
     </div>
   </div>
 </template>
+<script>
+export default {
+  props: {
+    stock: {
+      type: Object,
+      default: () => {
+        return {
+          id: 0,
+          name: "",
+          price: 0
+        };
+      }
+    }
+  },
+  data() {
+    return {
+      quantity: ''
+    }
+  },
+  computed: {
+    isValidQuantity() {
+      return this.quantity && this.quantity > 0;
+    }
+  }
+};
+</script>
--- stock-trader/src/components/stocks/Stocks.vue
+++ stock-trader/src/components/stocks/Stocks.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Stocks</h1>
     <div class="row">
-      <stock-card class="col mb-3" v-for="(stock, index) in stocks" :key="index"></stock-card>
+      <stock-card class="col mb-3" v-for="(stock, index) in stocks" :key="index" :stock="stock"></stock-card>
     </div>
   </div>
 </template>
